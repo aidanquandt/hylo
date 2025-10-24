be8f368efc5ee964bc8a8e969dc4fa3cfe1e2a97
Aidan Quandt <aidanq2003@gmail.com>
2025-10-23 23:52:24 -0700

    tcm working

--- firmware/components/platform/platform.h
+++ firmware/components/platform/platform.h
@@ -1,5 +1,16 @@
 #pragma once
 
+
+// todo - move this to somewhere else
+
+#ifdef __GNUC__
+#define TCM_FUNCTION __attribute__((section(".itcmram")))
+#define TCM_VARIABLE __attribute__((section(".dtcmram")))
+#else
+#define TCM_FUNCTION
+#define TCM_VARIABLE
+#endif
+
 #include <stdint.h>
 
 uint32_t platform_gettick(void);
